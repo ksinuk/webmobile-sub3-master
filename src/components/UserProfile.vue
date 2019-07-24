fbd64e5869a7cf648f1e95e6d807ef66d8a89efb
Hwang Yeojin <hwangyj0202@gmail.com>
2019-07-24 13:37:24 +0900

    0724|user page 내 북마크 리스트

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -32,13 +32,53 @@
       </v-flex>
     </v-layout>
     <!-- user book mark list-->
-    
+    <div class="my-5">
+      <p style="font-size: 2rem;">BOOKMARK</p>
+      <v-layout row wrap>
+          <v-flex>
+              <carousel per-page="3">
+                  <slide v-for="bookmark in bookmarkList" class="px-2">
+                      <v-card>
+                          <v-img
+                          :src="bookmark.img"
+                          height="200px"
+                          >
+                          </v-img>
+
+                          <v-card-title primary-title>
+                          <div>
+                              <div class="headline">{{ bookmark.title }}</div>
+                              <div>
+                                <tr>
+                                  <td v-for="hashtag in bookmark.hashtags">
+                                    <v-chip color="teal" text-color="white">
+                                      <v-avatar>
+                                          <v-icon>check_circle</v-icon>
+                                      </v-avatar>
+                                      {{ hashtag }}
+                                    </v-chip>
+                                  </td>
+                                </tr>
+                              </div>
+                          </div>
+                          </v-card-title>
+                          <v-card-actions>
+                              <v-btn flat color="purple">Explore</v-btn>
+                              <v-spacer></v-spacer>
+                          </v-card-actions>
+                      </v-card>
+                  </slide>
+              </carousel>
+          </v-flex>
+      </v-layout>
+    </div>
     <!-- user edit modal -->
   </div>
 </template>
 
 <script>
 import firebase from 'firebase'
+import FirebaseServices from '../services/FirebaseServices'
 import UserProfileBtn from './UserProfileBtn.vue'
 import { Carousel, Slide } from 'vue-carousel';
 
@@ -55,14 +95,26 @@ export default {
     return {
       dialog: false,
       photoURL: this.$store.state.firebaseUser.photoURL,
+      bookmarkList: []
     }
   },
   created() {
     this.viewProfile()
+    this.viewBookmark()
   },
   methods: {
     viewProfile: function() {
       this.photoURL = this.$store.state.firebaseUser.photoURL
+    },
+    async viewBookmark() {
+      var bookmarks = await FirebaseServices.getPortfolios();
+      var user = await FirebaseServices.currentUser();
+      for (let book in bookmarks) {
+        if (user.bookmark.includes(bookmarks[book].pk)) {
+          this.bookmarkList.push(bookmarks[book])
+        }
+      }
+      console.log(this.bookmarkList)
     }
   }
 }
