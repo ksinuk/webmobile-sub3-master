562bcb5606e55d572b5bb1433a9389eeb670835e
서지훈 <seodosa5@gmail.com>
2019-07-24 17:22:04 +0900

    portfolio percent method

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -12,20 +12,20 @@
           </v-avatar>
           <div>
             <v-btn small>My portfolio</v-btn>
-            
+
             <UserProfileBtn :photoURL="photoURL" :displayName="displayName"/>
 
           </div>
         </v-card>
       </v-flex>
-      
+
       <v-flex md8>
         <v-card>
           <v-card-text>유저 상태정보(권한 등)</v-card-text>
           <v-card-title primary-title>
             <div>
               <h3 class="headline mb-0">portfolio 제작 정도</h3>
-              <div>내용물</div>
+              <div>{{percent}}</div>
             </div>
           </v-card-title>
         </v-card>
@@ -84,6 +84,44 @@ import { Carousel, Slide } from 'vue-carousel';
 
 // import UserProfileBtn from './UserProfileTestBtn.vue'
 
+
+/*
+import FirebaseService from '@/services/FirebaseServices'
+import PortfolioList from '@/components/PortfolioList.vue'
+import Introduce from '@/components/Introduce.vue'
+
+export default {
+
+    name: 'portfolio',
+    components: {
+        ImageBanner,
+        PortfolioList,
+        Introduce
+    },
+    data(){
+        return {
+            portfolios:[],
+            intros:[],
+            introp:[],
+            css:1,
+
+        }
+    },
+
+    mounted(){
+        this.getMyPortfolio()
+        this.getMyIntro()
+    },
+    methods :{
+        async getMyPortfolio(){
+      //  var user = FirebaseService.auth().currentUser;
+        //유저정보에 따라 구현
+        this.portfolios = await FirebaseService.getPortfolios()
+
+
+
+      },
+*/
 export default {
   name: 'UserProfile',
   components: {
@@ -95,13 +133,19 @@ export default {
     return {
       dialog: false,
       photoURL: this.$store.state.firebaseUser.photoURL,
-      bookmarkList: []
+      bookmarkList: [],
+      percent: 0
     }
   },
   created() {
     this.viewProfile()
     this.viewBookmark()
+
   },
+  mounted(){
+      this.getPortfolio()
+  }
+  ,
   methods: {
     viewProfile: function() {
       this.photoURL = this.$store.state.firebaseUser.photoURL
@@ -115,7 +159,48 @@ export default {
         }
       }
       console.log(this.bookmarkList)
+    },
+    async getPortfolio(){
+      console.log('test')
+      var user = await FirebaseServices.currentUser();
+      if(user){
+        console.log(user.uid);
+      }
+      else{
+        console.log('유저정보 가져오기 실패');
+      }
+      let portfolio = await FirebaseServices.getPortfolio(user.uid);
+      console.log("test"+portfolio.type)
+      let count =0;
+      let totalCount =3;
+      totalCount+= (8*portfolio.portfolios.length);
+      totalCount+= (3*portfolio.skills.length);
+      if(portfolio.aboutme!='') count++;
+      if(portfolio.hashtags.length!=0) count++;
+      if(portfolio.type!=0) count++;
+      let temp = portfolio.portfolios;
+      for(let i=0; i<portfolio.portfolios.length;i++){
+        if(temp[i].content!='') count++;
+        if(temp[i].demo_url!='') count++;
+        if(temp[i].git.length!=0) count++;
+        if(temp[i].ie_support!='') count++;
+        if(temp[i].img!='') count++;
+        if(temp[i].repos_url!='') count++;
+        if(temp[i].title!='') count++;
+        if(temp[i].viewport!='') count++;
+      }
+      let temp2 = portfolio.skills;
+      for(let i=0; i<temp2.length;i++){
+        if(temp2[i].description !='') count++;
+        if(temp2[i].level !=0) count++;
+        if(temp2[i].skillname !='') count++;
+      }
+      console.log(count);
+      console.log(totalCount);
+      this.percent = (count/totalCount)*100;
+      console.log(this.percent);
     }
+
   }
 }
 </script>
