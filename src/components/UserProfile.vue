52d3fde6795148639bf122d772ad44155c27bf89
kang sinuk <ghttttgu@naver.com>
2019-07-26 11:00:08 +0900

    temp

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,85 +1,93 @@
 <template>
-  <div>
+<div>
     <!-- user profile -->
     <v-layout row mt-5>
-      <v-flex md4>
-        <v-card flat color="transparent">
-          <v-avatar
-            size="256"
-            class="profileImg"
-          >
-            <img :src="photoURL ? photoURL:this.$store.state.imgSrc.noImgSrc" alt="avatar">
-          </v-avatar>
-          <div>
-            <v-btn small>My portfolio</v-btn>
-
-            <UserProfileBtn :photoURL="photoURL" :displayName="displayName"/>
-
-          </div>
-        </v-card>
-      </v-flex>
-
-      <v-flex md8>
-        <v-card>
-          <v-card-text>유저 상태정보(권한 등)</v-card-text>
-          <v-card-title primary-title>
+        <v-flex md4>
+            <v-card flat color="transparent">
+            <v-avatar size="256" class="profileImg">
+                <img :src="photoURL ? photoURL:this.$store.state.imgSrc.noImgSrc" alt="avatar">
+            </v-avatar>
             <div>
-              <h3 class="headline mb-0">포트폴리오 제작 현황</h3>
+                <v-btn small>My portfolio</v-btn>
+                
+                <UserProfileBtn :photoURL="photoURL" :displayName="displayName"/>
 
-              <div id = "cont">
-                <svg viewPort="0 0 320 320" width="320" height="320" id="svg" xmlns="http://www.w3.org/2000/svg">
-                <circle class="cir" cx="160" cy="160" r="100" stroke="#5895DC" fill="none" stroke-width="10" stroke-linecap="round" ></circle>
-              </svg>
-              <span id="mypercent">{{percent}}%</span>
-</div>
             </div>
-          </v-card-title>
-        </v-card>
-      </v-flex>
+            </v-card>
+        </v-flex>
+        
+        <div class="green-box">
+            <div class="green-box-part-left">
+                <h1>완료정도</h1>
+                <div class="complate-box">
+                    <div class="back-circle"></div>
+                    <canvas class="front-circle"></canvas>
+                    <h3 class="text-circle">{{complatePercent+'%'}}</h3>
+                </div>
+            </div>
+            <div class="green-box-part-right">
+                <h1>이미지 저장소</h1>
+                <div class="img-box">
+                    <i class="fas fa-database icon-db"></i>
+                    <i class="fas fa-angle-double-down icon-down"></i>
+                    <i class="fas fa-angle-double-up icon-up"></i>
+                </div>
+            </div>
+            <div class="text-box">
+                <p>Text - Double click to edit</p>
+                <div class="quest-box">
+                    <h4>안녕하세요</h4>                
+                </div>
+                <div class="quest-tail">              
+                </div>
+            </div>
+        </div>
+        
     </v-layout>
+    <div class="file-list">
+        <h1>청춘에서만 구할 수</h1>
+    </div>
     <!-- user book mark list-->
     <div class="my-5">
-      <p style="font-size: 2rem;">BOOKMARK</p>
-      <v-layout row wrap>
-          <v-flex>
-              <carousel per-page="3">
-                  <slide v-for="bookmark in bookmarkList" class="px-2">
-                      <v-card>
-                          <v-img
-                          :src="bookmark.img"
-                          height="200px"
-                          >
-                          </v-img>
+        <p style="font-size: 2rem;">BOOKMARK</p>
+        <v-layout row wrap>
+            <v-flex>
+                <carousel per-page="3">
+                    <slide v-for="bookmark in bookmarkList" class="px-2">
+                        <v-card>
+                            <v-img :src="bookmark.img" height="200px">
+                            </v-img>
 
-                          <v-card-title primary-title>
-                          <div>
-                              <div class="headline">{{ bookmark.title }}</div>
-                              <div>
-                                <tr>
-                                  <td v-for="hashtag in bookmark.hashtags">
-                                    <v-chip color="teal" text-color="white">
-                                      <v-avatar>
-                                          <v-icon>check_circle</v-icon>
-                                      </v-avatar>
-                                      {{ hashtag }}
-                                    </v-chip>
-                                  </td>
-                                </tr>
-                              </div>
-                          </div>
-                          </v-card-title>
-                          <v-card-actions>
-                              <v-btn flat color="purple">Explore</v-btn>
-                              <v-spacer></v-spacer>
-                          </v-card-actions>
-                      </v-card>
-                  </slide>
-              </carousel>
-          </v-flex>
-      </v-layout>
+                            <v-card-title primary-title>
+                                <div>
+                                    <div class="headline">{{ bookmark.title }}</div>
+                                    <div>
+                                        <tr>
+                                        <td v-for="hashtag in bookmark.hashtags">
+                                            <v-chip color="teal" text-color="white">
+                                            <v-avatar>
+                                                <v-icon>check_circle</v-icon>
+                                            </v-avatar>
+                                            {{ hashtag }}
+                                            </v-chip>
+                                        </td>
+                                        </tr>
+                                    </div>
+                                </div>
+                            </v-card-title>
+                            <v-card-actions>
+                                <v-btn flat color="purple">Explore</v-btn>
+                                <v-spacer></v-spacer>
+                            </v-card-actions>
+                        </v-card>
+                    </slide>
+                </carousel>
+            </v-flex>
+        </v-layout>
     </div>
+    
     <!-- user edit modal -->
-  </div>
+</div>
 </template>
 
 <script>
@@ -88,117 +96,245 @@ import FirebaseServices from '../services/FirebaseServices'
 import UserProfileBtn from './UserProfileBtn.vue'
 import { Carousel, Slide } from 'vue-carousel';
 
+// import UserProfileBtn from './UserProfileTestBtn.vue'
+
 export default {
-  name: 'UserProfile',
-  components: {
-    UserProfileBtn,
-    Carousel,
-    Slide
-  },
-  data () {
-    return {
-      dialog: false,
-      photoURL: this.$store.state.firebaseUser.photoURL,
-      bookmarkList: [],
-      percent: 0
-    }
-  },
-  created() {
-    this.viewProfile()
-    this.viewBookmark()
-
-  },
-  mounted(){
-      this.getPortfolio()
-  }
-  ,
-  methods: {
-    viewProfile: function() {
-      this.photoURL = this.$store.state.firebaseUser.photoURL
+    name: 'UserProfile',
+    components: {
+        UserProfileBtn,
+        Carousel,
+        Slide
     },
-    async viewBookmark() {
-      var bookmarks = await FirebaseServices.getPortfolios();
-      var user = await FirebaseServices.currentUser();
-      for (let book in bookmarks) {
-        if (user.bookmark.includes(bookmarks[book].pk)) {
-          this.bookmarkList.push(bookmarks[book])
+    data () {
+        return {
+            dialog: false,
+            photoURL: this.$store.state.firebaseUser.photoURL,
+            complatePercent:70,
+            bookmarkList: []
         }
-      }
-      console.log(this.bookmarkList)
     },
-    async getPortfolio(){
-      console.log('test')
-      var user = await FirebaseServices.currentUser();
-      if(user){
-        console.log(user.uid);
-      }
-      else{
-        console.log('유저정보 가져오기 실패');
-      }
-      let portfolio = await FirebaseServices.getPortfolio(user.uid);
-      console.log("test"+portfolio.type)
-      let count =0;
-      let totalCount =3;
-      totalCount+= (8*portfolio.portfolios.length);
-      totalCount+= (3*portfolio.skills.length);
-      if(portfolio.aboutme!='') count++;
-      if(portfolio.hashtags.length!=0) count++;
-      if(portfolio.type!=0) count++;
-      let temp = portfolio.portfolios;
-      for(let i=0; i<portfolio.portfolios.length;i++){
-        if(temp[i].content!='') count++;
-        if(temp[i].demo_url!='') count++;
-        if(temp[i].git.length!=0) count++;
-        if(temp[i].ie_support!='') count++;
-        if(temp[i].img!='') count++;
-        if(temp[i].repos_url!='') count++;
-        if(temp[i].title!='') count++;
-        if(temp[i].viewport!='') count++;
-      }
-      let temp2 = portfolio.skills;
-      for(let i=0; i<temp2.length;i++){
-        if(temp2[i].description !='') count++;
-        if(temp2[i].level !=0) count++;
-        if(temp2[i].skillname !='') count++;
-      }
-      console.log(count);
-      console.log(totalCount);
-      this.percent = Math.round((count/totalCount)*100);
-      console.log(this.percent);
-      var val = this.percent;
+    created() {
+        this.viewProfile()
+        this.viewBookmark()
+    },
+    mounted(){
+        this.makeChart()
+        this.getPortfolio()
+    },
+    methods: {
+        viewProfile: function() {
+            this.photoURL = this.$store.state.firebaseUser.photoURL
+        },
+        makeChart: function(){
+            var cc = document.querySelector('.front-circle')
+            let r = 75
+            var ctx = cc.getContext("2d")
+            ctx.beginPath();
+            ctx.arc(r*2, r, r-10, 2 * Math.PI*0.75, 2 * Math.PI*(this.complatePercent/100-0.25));
+            ctx.lineWidth = 20
+            ctx.strokeStyle = 'blue'
+            ctx.stroke();
+        },
+        async viewBookmark() {
+            var bookmarks = await FirebaseServices.getPortfolios();
+            var user = await FirebaseServices.currentUser();
+            for (let book in bookmarks) {
+                if (user.bookmark.includes(bookmarks[book].pk)) {
+                this.bookmarkList.push(bookmarks[book])
+                }
+            }
+            console.log(this.bookmarkList)
+        },
 
-    var $circle = $('#svg .cir');
-    var r = $circle.attr('r');
-    var per = Math.round(((100 - val) / 100) * Math.PI * r * 2);
+        async getPortfolio(){
+        console.log('test')
+        var user = await FirebaseServices.currentUser();
+        if(user){
+            console.log(user.uid);
+        }
+        else{
+            console.log('유저정보 가져오기 실패');
+        }
+        let portfolio = await FirebaseServices.getPortfolio(user.uid);
+        console.log("test"+portfolio.type)
+        let count =0;
+        let totalCount =3;
+        totalCount+= (8*portfolio.portfolios.length);
+        totalCount+= (3*portfolio.skills.length);
+        if(portfolio.aboutme!='') count++;
+        if(portfolio.hashtags.length!=0) count++;
+        if(portfolio.type!=0) count++;
+        let temp = portfolio.portfolios;
+        for(let i=0; i<portfolio.portfolios.length;i++){
+            if(temp[i].content!='') count++;
+            if(temp[i].demo_url!='') count++;
+            if(temp[i].git.length!=0) count++;
+            if(temp[i].ie_support!='') count++;
+            if(temp[i].img!='') count++;
+            if(temp[i].repos_url!='') count++;
+            if(temp[i].title!='') count++;
+            if(temp[i].viewport!='') count++;
+        }
+        let temp2 = portfolio.skills;
+        for(let i=0; i<temp2.length;i++){
+            if(temp2[i].description !='') count++;
+            if(temp2[i].level !=0) count++;
+            if(temp2[i].skillname !='') count++;
+        }
+        console.log(count);
+        console.log(totalCount);
+        this.percent = Math.round((count/totalCount)*100);
+        console.log(this.percent);
+        var val = this.percent;
 
-    $circle.css({
-      strokeDashoffset: per
-    });
+        var $circle = $('#svg .cir');
+        var r = $circle.attr('r');
+        var per = Math.round(((100 - val) / 100) * Math.PI * r * 2);
+
+        $circle.css({
+        strokeDashoffset: per
+        });
+        }
+
     }
-
-  }
 }
 </script>
 
 <style>
-.profileImg {
-  margin: 10px;
-}
-#svg .cir{
-  stroke-dasharray: 628;
-  stroke-dashoffset:628;
-  transition: stroke-dashoffset 1s linear;
-}
-#svg {
-   transform: rotate(-90deg);
-}
+    .profileImg {
+        margin: 10px;
+    }
 
-#mypercent {
-  position: absolute;
-  margin-top: 3.4em;
-  margin-left: -4.7em;
-  text-align: center;
-  font-size: 40px;
+    .green-box{
+        background-color: rgb(155, 214, 203);
+        width:600px;
+    }
 
-}
+    .green-box-part-left,.green-box-part-right{
+        width:44%;
+        display: inline-block;
+    }
+    .green-box-part-left{
+        margin:10px 2% 10px 4%;
+    }
+    .green-box-part-right{
+        margin:10px 4% 10px 2%;
+    }
+    .green-box-part-left h1 , .green-box-part-right h1{
+        margin:10px 0;
+        text-align: left;
+        font-size:22px;
+    }
+
+    .text-box > p{
+        text-align: left;
+        margin-left:4%;
+        font-size:18px;
+    }
+    .quest-box{
+        background-color: #666;
+        margin:0 4%;
+        height:60px;
+        position: relative;
+        border-radius: 10px;
+    }
+    .quest-box h4{
+        color:white;
+        font-size:20px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+    .quest-tail{
+        margin:0 4% 20px 10%;
+        width: 0px;height: 0px;
+        border-bottom:20px solid none;
+        border-top:20px solid #666;
+        border-right: 20px solid transparent;
+        border-left: 20px solid  transparent;
+    }
+
+    .complate-box, .img-box{
+        width:100%;
+        height:250px;
+        background-color: rgb(26, 188, 156);
+        display:inline-block;
+        position: relative;
+    }
+    .img-box .icon-db{
+        font-size:100px;
+        margin-top:50px;
+        position: absolute;
+        top:0;
+        left:50%;
+        transform: translateX(-50%)
+    }
+    .img-box .icon-up{
+        font-size:50px;
+        margin-bottom:20px;
+        position: absolute;
+        bottom:0;
+        left:60%;
+        transform: translateX(-50%)
+    }
+    .img-box .icon-down{
+        font-size:50px;
+        margin-bottom:20px;
+        position: absolute;
+        bottom:0;
+        left:40%;
+        transform: translateX(-50%)
+    }
+
+    .file-list{
+        margin-top:50px;
+    }
+
+    /*  chart ------------------------ */
+    .back-circle, .front-circle, .text-circle{
+        display:block;
+        position:absolute;
+        top:50%;
+        left:50%;
+        transform: translate(-50%,-50%);
+        margin:0;
+    }
+    .text-circle{
+        font-size:30px;
+    }
+    .back-circle{
+        border:20px solid gray;
+        height:150px;
+        width:150px;
+        border-radius: 75px;
+    }
+    .front-circle{
+        height:150px;
+        width:300px;
+        border-radius: 75px;
+    }
+    
+    
+    .profileImg {
+        margin: 10px;
+    }
+    #svg .cir{
+        stroke-dasharray: 628;
+        stroke-dashoffset:628;
+        transition: stroke-dashoffset 1s linear;
+    }
+    #svg {
+        transform: rotate(-90deg);
+    }
+
+    #mypercent {
+        position: absolute;
+        margin-top: 3.4em;
+        margin-left: -4.7em;
+        text-align: center;
+        font-size: 40px;
+
+    }
 </style>
