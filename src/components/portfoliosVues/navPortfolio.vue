194bb9a983d5ed8de64a798e15d234a2ae0cae3d
Hwang Yeojin <hwangyj0202@gmail.com>
2019-08-05 17:45:57 +0900

    0805|포트폴리오 컨텐츠 수정

--- src/components/portfoliosVues/navPortfolio.vue
+++ src/components/portfoliosVues/navPortfolio.vue
@@ -1,27 +1,27 @@
 <template>
-  <v-navigation-drawer
-    v-model="drawer" :mini-variant="mini"
-    fixed hide-overlay disable-route-watcher
-  >
-    <v-toolbar flat class="transparent">
-    </v-toolbar>
+    <v-navigation-drawer
+      v-model="drawer" :mini-variant="mini"
+      fixed hide-overlay disable-route-watcher
+    >
+      <v-toolbar flat class="transparent">
+      </v-toolbar>
 
-    <v-list class="pt-0" dense>
-      <v-divider></v-divider>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
 
-      <v-list-tile
-        v-for="item in items"
-        :key="item.title"
-        @click=""
-      >
-        <v-list-tile-action>
-          <!-- eventBus를 활용한 데이터 통신 -->
-          <v-btn icon @click="drawerHandler(item.title)"><v-icon>{{ item.icon }}</v-icon></v-btn>
-        </v-list-tile-action>
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+          @click=""
+        >
+          <v-list-tile-action>
+            <!-- eventBus를 활용한 데이터 통신 -->
+            <v-btn icon @click="drawerHandler(item.title)"><v-icon>{{ item.icon }}</v-icon></v-btn>
+          </v-list-tile-action>
 
-      </v-list-tile>
-    </v-list>
-  </v-navigation-drawer>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
 </template>
 
 <script>
@@ -32,7 +32,7 @@ export default {
       items: [
         { title: 'Header', icon: 'fas fa-home' },
         { title: 'About', icon: 'fas fa-user-edit' },
-        { title: 'Portfolio', icon: 'fas fa-file-code' },
+        { title: 'Portfolio', icon: 'fas fa-file-code' }
       ],
       drawer: true,
       mini: true,
@@ -45,8 +45,11 @@ export default {
     }
   }
 }
+
 function templateMove(location){
+  console.log(location)
   var offset = $("#custom" + location).offset();
+  console.log(offset)
   $('html, body').animate({scrollTop : offset.top}, 100);
 }
 </script>
--- src/views/PortfolioPage.vue
+++ src/views/PortfolioPage.vue
@@ -1,6 +1,6 @@
 <template lang="html">
 <div class="portfolio">
-  <NavPortfolio/>
+  <NavPortfolio v-if="user === item.uid"/>
   <Header id="customHeader" :uid="uid"/>
   <AboutMe id="customAbout" :uid="uid"/>
   <PortfolioList id="customPortfolio" :uid="uid" :css="css"/>
@@ -12,7 +12,7 @@ import Header from '@/components/portfoliosVues/Header.vue'
 import AboutMe from '@/components/portfoliosVues/AboutMe.vue'
 import PortfolioList from '@/components/portfoliosVues/PortfolioList.vue'
 import NavPortfolio from '@/components/portfoliosVues/navPortfolio.vue'
-import FirebaseService from '../services/FirebaseServices'
+import FirebaseServices from '../services/FirebaseServices'
 import firebase from 'firebase/app'
 
 export default {
@@ -24,8 +24,6 @@ export default {
         PortfolioList,
         NavPortfolio
     },
-
-
     created() {
       this.getUserID();
       let th = this;
@@ -51,6 +49,7 @@ export default {
         let __this = this;
         var user = firebase.auth().currentUser;
 
+        // user login 정보가 없을 때 로그인 페이지로 redirect
         if (!user) {
           alert('로그인이 필요합니다!');
 
@@ -58,11 +57,13 @@ export default {
         }
         const tmp = firebase.auth().onAuthStateChanged(function(user) {
             __this.user = user.uid;
-
+            console.log(__this.user);
 
             FirebaseServices.getMyPort(user.uid).then(function(res) {
                 __this.item = res;
-                console.log("test"+res);
+                console.log(__this.item);
+
+                // 저장된 portfolio 페이지가 없을 때에 포트폴리오 입력 페이지로 redirect
                 if(res==null){
                   alert('저장된 포트폴리오 정보가 없습니다!')
 
