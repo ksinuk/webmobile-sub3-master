0f851d5b56a4ae01b1e6a5c30283052f0d634603
kang sinuk <ghttttgu@naver.com>
2019-08-09 10:07:54 +0900

    del dumy func

--- src/components/SideBarItems.vue
+++ src/components/SideBarItems.vue
@@ -1,72 +1,68 @@
 <template>
-  <v-container id="sideCtn">
-    <!-- vuex에서 유저 이름을 가져와서 router로 연결 -->
-    <!-- 로그인 상태확인 해서 보여줌 -->
-    <div v-show="this.$store.state.firebaseUser.inUser">
-      <img :src="this.$store.state.firebaseUser.photoURL" style="border-radius: 50%; height: 8rem; width: 8rem;">
-      <h1 style="padding-top: 1rem; color: white;">{{ this.$store.state.firebaseUser.name }}</h1>
-      <v-btn small @click="logoutUser" color="error">Logout</v-btn>
-      <v-divider/>
-      <v-btn active-class="active" flat block :to="{name: 'userpage', params: { userId: this.$store.state.firebaseUser.name }}">My Page</v-btn>
-    </div>
-    <div v-show="!this.$store.state.firebaseUser.inUser">
-      <!-- 로그인 되어 있으면 가림 -->
-      <v-btn small color="info" to="/login">Login</v-btn>
-      <v-divider/>
-    </div>
-    <div>
-      <v-btn active-class="active" flat block to="/board">board</v-btn>
-      <v-btn active-class="active" flat block to="/portfoliopage">my portfolio</v-btn>
-      <v-btn active-class="active" flat block to="/mainPortfoliopage">other portfolio</v-btn>
-      <!-- 제공되는 검색 기능 -->
-      <v-flex>
-        <v-text-field
-          label="Search"
-          v-model="searchItem"
-          v-on:keyup.enter="findItem"
-          color="white"
-          ></v-text-field>
-      </v-flex>
-      <div>
-        <v-btn round depressed small>임베디드</v-btn>
-      </div>
-    </div>
-  </v-container>
+    <v-container id="sideCtn">
+        <!-- vuex에서 유저 이름을 가져와서 router로 연결 -->
+        <!-- 로그인 상태확인 해서 보여줌 -->
+        <div v-show="this.$store.state.firebaseUser.inUser">
+            <img :src="this.$store.state.firebaseUser.photoURL" style="border-radius: 50%; height: 8rem; width: 8rem;">
+            <h1 style="padding-top: 1rem; color: white;">{{ this.$store.state.firebaseUser.name }}</h1>
+            <v-btn small @click="logoutUser" color="error">Logout</v-btn>
+            <v-divider/>
+            <v-btn active-class="active" flat block :to="{name: 'userpage', params: { userId: this.$store.state.firebaseUser.name }}">My Page</v-btn>
+        </div>
+        <div v-show="!this.$store.state.firebaseUser.inUser">
+            <!-- 로그인 되어 있으면 가림 -->
+            <v-btn small color="info" to="/login">Login</v-btn>
+            <v-divider/>
+        </div>
+        <div>
+            <v-btn active-class="active" flat block to="/board">board</v-btn>
+            <v-btn active-class="active" flat block to="/portfoliopage">my portfolio</v-btn>
+            <v-btn active-class="active" flat block to="/mainPortfoliopage">other portfolio</v-btn>
+            <!-- 제공되는 검색 기능 -->
+            <v-flex>
+                <v-text-field label="Search" v-model="searchItem" v-on:keyup.enter="findItem" color="white" ></v-text-field>
+            </v-flex>
+            <div>
+                <v-btn style="backgroundColor:red;" round depressed small >임베디드</v-btn>
+            </div>
+        </div>
+    </v-container>
 </template>
 
 <script>
 import FirebaseServices from "../services/FirebaseServices";
 
 export default {
-  name: "sideBarItems",
-  data() {
-    return {
-      userImg: null,
-      searchItem: null
-    }
-  },
-  methods: {
-    closeDrawer(trigger) {
-      this.$EventBus.$emit(trigger)
+    name: "sideBarItems",
+    data() {
+        return {
+            userImg: null,
+            searchItem: null
+        }
     },
-    logoutUser() {
-      FirebaseServices.logoutUser()
-      this.closeDrawer('Logout')
-    },
-    findItem: function() {
-      console.log(this.searchItem)
-      this.$router.push("/search/" + this.searchItem)
-      window.location.reload()
+    methods: {
+        closeDrawer(trigger) {
+            this.$EventBus.$emit(trigger)
+        },
+        logoutUser() {
+            FirebaseServices.logoutUser()
+            this.closeDrawer('Logout')
+        },
+        findItem: function() {
+            console.log(this.searchItem)
+            this.$router.push("/search/" + this.searchItem)
+            window.location.reload()
+        }
     }
-  }
 };
 </script>
 
 <style scoped>
 #sideCtn {
-  margin-top: 70px;
+     margin-top: 70px;
 }
 .active {
-  background-color: #4caf50;
+    background-color: #4caf50;
 }
 </style>
+
