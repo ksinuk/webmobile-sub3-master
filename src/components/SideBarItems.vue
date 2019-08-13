4cdbb6530962a13f0d7691a604af9816d58e6054
전영진 <kimsuhan2483@gmail.com>
2019-08-13 13:21:48 +0900

    0813 | sprint4 | sidebar user image fix

--- src/components/SideBarItems.vue
+++ src/components/SideBarItems.vue
@@ -3,7 +3,9 @@
         <!-- vuex에서 유저 이름을 가져와서 router로 연결 -->
         <!-- 로그인 상태확인 해서 보여줌 -->
         <div v-if="this.$store.state.firebaseUser.inUser">
-            <img :src="this.$store.state.firebaseUser.photoURL" style="border-radius: 50%; height: 8rem; width: 8rem;">
+            <img v-show="userImg.bool" :src="userImg.src" style="border-radius: 50%; height: 8rem; width: 8rem;">
+            <img v-show="!userImg.bool" :src="this.$store.state.firebaseUser.photoURL" style="border-radius: 50%; height: 8rem; width: 8rem;">
+            
             <h1 style="padding-top: 1rem; color: white;">{{ this.$store.state.firebaseUser.name }}</h1>
             <v-btn small @click="logoutUser" color="error">Logout</v-btn>
             <v-divider/>
@@ -35,11 +37,16 @@
 
 <script>
 import FirebaseServices from "../services/FirebaseServices";
+import store from '../store'
 
 export default {
     name: "sideBarItems",
     data() {
         return {
+            userImg: {
+                bool: false,
+                src: null
+            },
             searchItem: '',
             tagDict: {},
             items: []
@@ -53,12 +60,11 @@ export default {
         for (let item in this.tagDict) {
             this.items.push(item);
         }
-
         this.$EventBus.$on('changePhoto', (URL) => {
-            this.userImg = URL
+            store.commit('setPhotoURL', URL)
+            this.userImg.bool = true
+            this.userImg.src = URL
         })
-
-        // console.log("this.$store : ", this.$store)
     },
     methods: {
         closeDrawer(trigger) {
